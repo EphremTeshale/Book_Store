7ee50bc65c75a76e2fc9a9c8f64a4db70499f0ce
Ephrem Teshale <ephremteshale@gmail.com>
2020-05-22 17:21:37 +0300

    ch10

---
+++ books/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    '''Admin View for Book'''
+
+    list_display = ('title','author','price',)
+    list_filter = ('author',)
+    search_fields = ('title','author',)
+    ordering = ('title',)
