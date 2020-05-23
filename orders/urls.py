85b8ec4b50d3b2d2a876d21ac622f88747633261
Ephrem Teshale <ephremteshale@gmail.com>
2020-05-23 07:40:39 +0300

    ch4

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 # orders/urls.py
 from django.urls import path
 
-from .views import OrderPageView
+from .views import OrderPageView, charge
 
 urlpatterns = [
     # Django admin
+    path('charge/', charge, name='charge'),
     path('', OrderPageView.as_view(), name='orders'),
 ]
 
